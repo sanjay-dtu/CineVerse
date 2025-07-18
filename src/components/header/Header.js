d95c92c189bc4cea059b45867203603a14ce4534
Sanjay Kumar <sanjaykumar@192.168.1.10>
2025-07-19 00:13:32 +0530

    Initial commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,10 +6,18 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <h2 style={{ color: "lightblue", fontWeight: "bold", marginRight: "20px" }}>CineVerse</h2>
+                </Link>
+                <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+                    <span>Popular</span>
+                </Link>
+                <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+                    <span>Top Rated</span>
+                </Link>
+                <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+                    <span>Upcoming</span>
+                </Link>
             </div>
         </div>
     )
